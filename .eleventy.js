d74edf1fc97410c8c365de5f8087f2bcd851693c
John Gallagher <john@synapticmishap.co.uk>
2024-02-04 14:57:03 +0000

    fix: filters are captalized

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
+const appConfig = {
+  anyFilter: "any"
+};
+
+
 Array.prototype.flatMap = function (callback) {
   return this.reduce((acc, cur) => {
     const mapped = callback(cur);
@@ -11,25 +16,28 @@ Array.prototype.uniq = function () {
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssClassesForShowingCategory", function (value) {
-    return value.concat(['any']).map(function (category) {
+    return value.concat([appConfig.anyFilter]).map(function (category) {
       return `data-[category=${category}]:block`;
     }).join(" ");
   });
   eleventyConfig.addFilter("searchFilterCategories", function (posts) {
-    return ['any'].concat(posts.flatMap(function (post) {
+    return [appConfig.anyFilter].concat(posts.flatMap(function (post) {
       return post.categories;
     }));
   });
   eleventyConfig.addFilter("searchFiltersSort", function (array) {
     return array.sort((a, b) => {
-      if (a === "any") return -1;
-      if (b === "any") return 1;
+      if (a === appConfig.anyFilter) return -1;
+      if (b === appConfig.anyFilter) return 1;
       
       return a.localeCompare(b);
     });
   });
   eleventyConfig.addFilter("uniq", function (array) {
     return array.uniq();
+  });
+  eleventyConfig.addFilter("humanize", function (str) {
+    return str.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
   });
 };
 
